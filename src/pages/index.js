654397eacfedb7189c6ca947605dac06562a8450
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-08-16 18:54:40 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import { DocSearch } from '@docsearch/react'
 
 const features = [
   {
